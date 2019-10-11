6c27e62ecc4dcea200b5d250f42fecb26b8b91eb
SheGe <szymon.sito@hotmail.com>
2019-10-11 19:13:41 +0200

    Create example project with dotnet core web app

---
+++ 02-dotnetcore-demo/MyDemoApp/Pages/Privacy.cshtml.cs
@@ -0,0 +1,19 @@
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.Extensions.Logging;
+
+namespace MyDemoApp.Pages
+{
+    public class PrivacyModel : PageModel
+    {
+        private readonly ILogger<PrivacyModel> _logger;
+
+        public PrivacyModel(ILogger<PrivacyModel> logger)
+        {
+            _logger = logger;
+        }
+
+        public void OnGet()
+        {
+        }
+    }
+}
