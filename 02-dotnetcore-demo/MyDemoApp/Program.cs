3077111ee167358392d1e7980067e83a2819ddf5
SheGe <szymon.sito@hotmail.com>
2019-10-11 20:56:18 +0200

    Add custom logging options

--- 02-dotnetcore-demo/MyDemoApp/Program.cs
+++ 02-dotnetcore-demo/MyDemoApp/Program.cs
@@ -1,5 +1,9 @@
+using System.IO;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using NLog.Extensions.Logging;
+using LogLevel = Microsoft.Extensions.Logging.LogLevel;
 
 namespace MyDemoApp
 {
@@ -7,11 +11,33 @@ namespace MyDemoApp
     {
         public static void Main(string[] args)
         {
-            CreateHostBuilder(args).Build().Run();
+            var path = Path.GetFullPath("nlog.config");
+            NLog.LogManager.LoadConfiguration(path);
+            try
+            {
+                CreateHostBuilder(args).Build().Run();
+            }
+            finally
+            {
+                NLog.LogManager.Shutdown();
+            }
         }
 
         public static IHostBuilder CreateHostBuilder(string[] args) =>
             Host.CreateDefaultBuilder(args)
+                .ConfigureLogging((ctx, logging) =>
+                {
+                    logging.ClearProviders();
+
+                    logging.AddConsole();
+
+                    if (ctx.HostingEnvironment.IsProduction())
+                    {
+                        logging.AddNLog();
+                    }
+
+                    logging.SetMinimumLevel(LogLevel.Trace);
+                })
                 .ConfigureWebHostDefaults(webBuilder =>
                 {
                     webBuilder.UseStartup<Startup>();
