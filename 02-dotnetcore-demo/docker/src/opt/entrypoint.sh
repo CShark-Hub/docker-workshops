d4c9003a251865131a6ce86a70cb28cbe31717fe
SheGe <szymon.sito@hotmail.com>
2019-10-23 21:40:46 +0200

    Allow to configure custom log directory

--- 02-dotnetcore-demo/docker/src/opt/entrypoint.sh
+++ 02-dotnetcore-demo/docker/src/opt/entrypoint.sh
@@ -1,5 +1,24 @@
 #!/usr/bin/env bash
 
-echo "Hello!!"
+set -eux
+
+echo "[Info] Hello!!"
+
+function configure_logging
+{
+    NLOG_CONFIG=/demo-app/nlog.config
+
+    if [ ! -f ${NLOG_CONFIG} ]; then
+        echo "[Error] Cannot find ${NLOG_CONFIG} file"
+        exit 1
+    fi
+
+    sed -i "s|variable name=\"logFolder\" value=\".*\"|variable name=\"logFolder\" value=\"${LOG_DIR}\"|g" $NLOG_CONFIG
+    echo "[Info] Logging directory configured"
+}
+
+if [ ! -z ${LOG_DIR} ]; then
+    configure_logging
+fi
 
 exec "$@"
